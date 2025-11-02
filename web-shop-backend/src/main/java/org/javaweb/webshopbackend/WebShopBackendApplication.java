6db5a9d7f85e8a5d2c2ce1a7926b1571390654c4
cuagain886 <843614387@qq.com>
2025-11-02 22:20:21 +0800

    update

---
+++ web-shop-backend/src/main/java/org/javaweb/webshopbackend/WebShopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.javaweb.webshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopBackendApplication.class, args);
+    }
+
+}
---
+++ web-shop-backend/src/test/java/org/javaweb/webshopbackend/WebShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaweb.webshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
